13b5d0513ad2f06d7924e4f275cae8d2623bdcb8
gipolanjcky <gipolan.jaquelyn@gmail.com>
2023-11-07 23:24:55 +0700

    Second commit

--- app/src/main/java/com/example/gipolanfinalprojectpwr/PlantCareTipsActivity.kt
+++ app/src/main/java/com/example/gipolanfinalprojectpwr/PlantCareTipsActivity.kt
@@ -1,4 +1,13 @@
 package com.example.gipolanfinalprojectpwr
 
-class PlantCareTipsActivity {
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlantCareTipsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.plant_care_tips)
+        supportActionBar?.title = "Plant Care Tips" // Optional, set the action bar title
+    }
 }
--- app/src/main/java/com/example/gipolanfinalprojectpwr/PlantHealthTrackerActivity.kt
+++ app/src/main/java/com/example/gipolanfinalprojectpwr/PlantHealthTrackerActivity.kt
@@ -1,4 +1,12 @@
 package com.example.gipolanfinalprojectpwr
 
-class PlantHealthTrackerActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlantHealthTrackerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.plant_health_tracker)
+        supportActionBar?.title = "Plant Health Tracker" // Optional, set the action bar title
+    }
 }
